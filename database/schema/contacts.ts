6d4e9b7f15f7fae1ed905ed90ca12d0ab0711f11
farosoyy13 <156523277+farosoyy13@users.noreply.github.com>
2026-05-24 14:35:52 +0300

    add: database/schema/contacts.ts

---
+++ database/schema/contacts.ts
@@ -0,0 +1,16 @@
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod/v4";
+
+export const contactsTable = pgTable("contacts", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  message: text("message").notNull(),
+  email: text("email"),
+  phone: text("phone"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const insertContactSchema = createInsertSchema(contactsTable).omit({ id: true, createdAt: true });
+export type InsertContact = z.infer<typeof insertContactSchema>;
+export type Contact = typeof contactsTable.$inferSelect;
